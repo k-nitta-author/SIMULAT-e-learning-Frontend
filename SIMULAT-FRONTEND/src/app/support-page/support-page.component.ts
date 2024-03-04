c6bf209d44245c0564b55d89736d0d0a56d8ecfa
k-nitta-author <k.nitta.author@gmail.com>
2024-03-04 12:12:10 +0800

    created and set up support-page components
    set up support page components and styled them
    
    - help center
    - contact form

--- SIMULAT-FRONTEND/src/app/support-page/support-page.component.ts
+++ SIMULAT-FRONTEND/src/app/support-page/support-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HelpCenterComponent } from './help-center/help-center.component';
+
+
 @Component({
   selector: 'app-support-page',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent, HelpCenterComponent],
   templateUrl: './support-page.component.html',
   styleUrl: './support-page.component.css'
 })
