365c11750b7d398119bb18d973f9c80efae43c1b
k-nitta-author <k.nitta.author@gmail.com>
2024-03-06 15:00:43 +0800

    added new sidebar component and added it to app component; did minor styling
    created new sidebar component to match the wireframes. styling still needs work.

--- SIMULAT-FRONTEND/src/app/app.component.ts
+++ SIMULAT-FRONTEND/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-
+import { SidebarNavigationComponent } from './sidebar-navigation/sidebar-navigation.component';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,8 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
             RouterOutlet,
             RouterModule,
             FooterComponent,
-            NavigationBarComponent],
+            NavigationBarComponent,
+            SidebarNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
