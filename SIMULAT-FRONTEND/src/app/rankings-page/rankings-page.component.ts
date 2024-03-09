2839797a4b469a3bd3f93f26c407f0e15f02bcfe
k-nitta-author <k.nitta.author@gmail.com>
2024-03-09 20:49:30 +0800

    generated rankings page component

---
+++ SIMULAT-FRONTEND/src/app/rankings-page/rankings-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rankings-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './rankings-page.component.html',
+  styleUrl: './rankings-page.component.css'
+})
+export class RankingsPageComponent {
+
+}
