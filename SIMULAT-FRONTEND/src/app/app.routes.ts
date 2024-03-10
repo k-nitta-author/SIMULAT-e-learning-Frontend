4a991436a820c4046643a1581988ac675393cf08
k-nitta-author <k.nitta.author@gmail.com>
2024-03-10 20:13:54 +0800

    did styling to various components

--- SIMULAT-FRONTEND/src/app/app.routes.ts
+++ SIMULAT-FRONTEND/src/app/app.routes.ts
@@ -7,16 +7,27 @@ import { AccountPageComponent } from './account-page/account-page.component';
 import { ClassDashboardPageComponent } from './class-dashboard-page/class-dashboard-page.component';
 import { CoursesPageComponent } from './courses-page/courses-page.component';
 
+
+import { CommunityHallComponent } from './community-hall/community-hall.component';
+import { ClassGradebookComponent } from './class-gradebook/class-gradebook.component';
+import { RankingsPageComponent } from './rankings-page/rankings-page.component';
+import { ActivitiesPageComponent } from './activities-page/activities-page.component';
+
 import { TermsOfServiceComponent } from './legal-pages/terms-of-service/terms-of-service.component';
 import { PrivacyPolicyComponent } from './legal-pages/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent},
     { path: 'inbox', component: InboxPageComponent},
     { path: 'support', component: SupportPageComponent},
     { path: 'account', component: AccountPageComponent},
-    { path: 'terms_of_service', component: TermsOfServiceComponent},
-    { path: 'privacy_policy', component: PrivacyPolicyComponent},
+    { path: 'legal/terms_of_service', component: TermsOfServiceComponent},
+    { path: 'legal/privacy_policy', component: PrivacyPolicyComponent},
     { path: 'dashboard', component: ClassDashboardPageComponent},
-    { path: 'courses',component: CoursesPageComponent}
+    { path: 'courses',component: CoursesPageComponent},
+    { path: 'dashboard/gradebook', component: ClassGradebookComponent},
+    { path: 'dashboard/rankings', component: RankingsPageComponent},
+    { path: 'dashboard/community-hall', component: CommunityHallComponent},
+    { path: 'dashboard/activities', component: ActivitiesPageComponent}
 ];
--- SIMULAT-FRONTEND/src/app/sidebar-navigation/sidebar-navigation.component.ts
+++ SIMULAT-FRONTEND/src/app/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-sidebar-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar-navigation.component.html',
   styleUrl: './sidebar-navigation.component.css'
 })
