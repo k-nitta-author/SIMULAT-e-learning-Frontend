714eb5971660b3ac9e1a5776d3593c1774afeeba
k-nitta-author <k.nitta.author@gmail.com>
2024-03-13 17:04:07 +0800

    generated a leaderboard table component

---
+++ SIMULAT-FRONTEND/src/app/rankings-page/leaderboard-table/leaderboard-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaderboard-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './leaderboard-table.component.html',
+  styleUrl: './leaderboard-table.component.css'
+})
+export class LeaderboardTableComponent {
+
+}
