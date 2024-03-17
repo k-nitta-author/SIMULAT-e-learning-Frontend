51d33736f90a4f840cfb69844e7f18147270f13e
k-nitta-author <k.nitta.author@gmail.com>
2024-03-17 19:09:45 +0800

    minor changes to styling

--- SIMULAT-FRONTEND/src/app/rankings-page/rankings-page.component.ts
+++ SIMULAT-FRONTEND/src/app/rankings-page/rankings-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+
+import { LeaderboardTableComponent } from './leaderboard-table/leaderboard-table.component';
+
 @Component({
   selector: 'app-rankings-page',
   standalone: true,
-  imports: [],
+  imports: [LeaderboardTableComponent],
   templateUrl: './rankings-page.component.html',
   styleUrl: './rankings-page.component.css'
 })
