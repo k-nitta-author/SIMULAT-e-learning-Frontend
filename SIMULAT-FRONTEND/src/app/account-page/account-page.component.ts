2ce96a8a3becd6115460dc54a4f8b72a81ddc523
k-nitta-author <k.nitta.author@gmail.com>
2024-03-17 19:09:17 +0800

    worked on account page and styled it

--- SIMULAT-FRONTEND/src/app/account-page/account-page.component.ts
+++ SIMULAT-FRONTEND/src/app/account-page/account-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { AdminInfoViewComponent } from './admin-info-view/admin-info-view.component';
+import { BadgeViewComponent } from './badge-view/badge-view.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+
+
 @Component({
   selector: 'app-account-page',
   standalone: true,
-  imports: [],
+  imports: [AdminInfoViewComponent, BadgeViewComponent, ProfileViewComponent],
   templateUrl: './account-page.component.html',
   styleUrl: './account-page.component.css'
 })
---
+++ SIMULAT-FRONTEND/src/app/account-page/badge-preview/badge-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-badge-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './badge-preview.component.html',
+  styleUrl: './badge-preview.component.css'
+})
+export class BadgePreviewComponent {
+
+}
