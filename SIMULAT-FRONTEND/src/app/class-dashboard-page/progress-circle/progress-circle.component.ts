db7488befc350e72ab2f60e07ca2cf90ab9edfa1
k-nitta-author <k.nitta.author@gmail.com>
2024-03-26 17:08:55 +0800

    progress circle given ability to change color based on value of progress varaiable

--- SIMULAT-FRONTEND/src/app/class-dashboard-page/progress-circle/progress-circle.component.ts
+++ SIMULAT-FRONTEND/src/app/class-dashboard-page/progress-circle/progress-circle.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-progress-circle',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './progress-circle.component.html',
-  styleUrl: './progress-circle.component.css'
+  styleUrl: './progress-circle.component.css',
+
 })
 export class ProgressCircleComponent {
+
+  progress = 11;
+
+  progress_text = "asd";
+
+
+  getProgressText(): string{
+    return this.progress.toString() + " %";
+  }
+
+  getColorClass(): string{
+
+    const colors=['less-complete', 'med-low-complete', 'med-complete', 'med-high-complete', 'more-complete'];
+
+    const index = Math.floor(this.progress / (100/ colors.length));
+
+    return colors[index] || colors[0]
+
+  }
+
 
 }
