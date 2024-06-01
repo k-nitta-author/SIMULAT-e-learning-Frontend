cece854e5f103e428a2d656cdbf68a367926356e
k-nitta-author <k.nitta.author@gmail.com>
2024-06-01 17:53:44 +0800

    Made Changes to Sidebar to align with wireframes.

--- SIMULAT-FRONTEND/src/app/sidebar-navigation/sidebar-navigation.component.ts
+++ SIMULAT-FRONTEND/src/app/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 
 
 @Component({
   selector: 'app-sidebar-navigation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar-navigation.component.html',
   styleUrl: './sidebar-navigation.component.css'
 })
 export class SidebarNavigationComponent {
+
+  isSidebarOpen = false;
+
+
+  toggleSidebar(){
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
 }
