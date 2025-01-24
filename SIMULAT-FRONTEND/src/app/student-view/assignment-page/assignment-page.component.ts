0c592fdbbe145b22afe00e5cc449aacb58759b48
k-nitta-author <k.nitta.author@gmail.com>
2025-01-24 18:16:46 +0800

    🚧:under construction: created new components for assignments
    - created assignment page to show various assignments.
    - did basic boilerplate coding

---
+++ SIMULAT-FRONTEND/src/app/student-view/assignment-page/assignment-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignment-page.component.html',
+  styleUrl: './assignment-page.component.css'
+})
+export class AssignmentPageComponent {
+
+}
