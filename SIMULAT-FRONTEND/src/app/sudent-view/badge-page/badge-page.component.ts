0100aedead8a1702860e236bf947c70591468b7a
k-nitta-author <k.nitta.author@gmail.com>
2025-01-24 18:18:42 +0800

    🚧:under construction: badges page component
    - created badge page to show and link to each badge.
    - did basic boilerplate coding

---
+++ SIMULAT-FRONTEND/src/app/sudent-view/badge-page/badge-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-badge-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './badge-page.component.html',
+  styleUrl: './badge-page.component.css'
+})
+export class BadgePageComponent {
+
+}
