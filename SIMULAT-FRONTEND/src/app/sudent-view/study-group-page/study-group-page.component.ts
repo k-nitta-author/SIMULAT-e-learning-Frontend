7cb13528d908dd816cb287ce8cf1fb6993b692f0
k-nitta-author <k.nitta.author@gmail.com>
2025-01-24 18:09:54 +0800

    🚧: Under Construction: new components for study groups
    Added new content for study groups:
    
    - Study Group Page: to list and navigate the various study groups
    - Study Group Detail: to show the details of individual study groups

---
+++ SIMULAT-FRONTEND/src/app/sudent-view/study-group-page/study-group-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-study-group-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './study-group-page.component.html',
+  styleUrl: './study-group-page.component.css'
+})
+export class StudyGroupPageComponent {
+
+}
