f022d4ce58c1c3ebe41c89ba326a95ed2e416f12
k-nitta-author <k.nitta.author@gmail.com>
2025-01-24 18:15:42 +0800

    🚧:under construction: course details
    - created course detail page to show details for each course.
    - did basic boilerplate coding

---
+++ SIMULAT-FRONTEND/src/app/student-view/course-details-page/course-details-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-details-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-details-page.component.html',
+  styleUrl: './course-details-page.component.css'
+})
+export class CourseDetailsPageComponent {
+
+  
+
+}
