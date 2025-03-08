85df5627f0666f3766e387ebcc1595a9d53ff81e
k-nitta-author <k.nitta.author@gmail.com>
2025-03-08 18:49:12 +0800

    ✨:Made Modifications: Now Challenge Pages Contact API

--- SIMULAT-FRONTEND/src/app/general/challenge-page/challenge-page.component.ts
+++ SIMULAT-FRONTEND/src/app/general/challenge-page/challenge-page.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Challenge } from '../interfaces/challenge';
+import { ChallengeService } from '../../backend-services/challenge.service';
+import { NgFor, NgIf, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-challenge-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, DatePipe],
   templateUrl: './challenge-page.component.html',
-  styleUrl: './challenge-page.component.css'
+  styleUrl: './challenge-page.component.css',
+  providers: [ChallengeService]
 })
-export class ChallengePageComponent {
+export class ChallengePageComponent implements OnInit {
+
+  challenges: Challenge[] = [];
+
+  constructor(private challengeService: ChallengeService) { }
+
+  ngOnInit(): void {
+    this.challengeService.getAllChallenges().subscribe(challenges => this.challenges = challenges);
+  }
 
 }
+
