cf10af08bb4c3d0c24dcc28d8206ace44c1129b9
k-nitta-author <k.nitta.author@gmail.com>
2025-03-21 18:24:54 +0800

    ⚠️:IN PROGRESS: Weavy Service Updated

--- SIMULAT-FRONTEND/src/app/backend-services/weavy.service.ts
+++ SIMULAT-FRONTEND/src/app/backend-services/weavy.service.ts
@@ -8,11 +8,10 @@ export class WeavyService {
   weavy = new Weavy();
 
   constructor() { 
-    this.weavy.url ="https://1845cba530934b15a37270a57712b558.weavy.io";
-    // The following key is a sample key for the USERS API
-    this.weavy.tokenFactory = async (refresh: Boolean) => "wyu_YmxTsYyugCXGeSD33FUUOhX6WmmDoD3Ha2XX"
+    this.weavy.url ="https://12001144a8f2439994a19bf10b0ba9c6.weavy.io/";
 
     // The following key is our own generated key - uncomment when backend for users is ready
-    // this.weavy.tokenFactory = async (refresh: Boolean) => "wys_OqTZZ6EniKdWT18wiu6p5d0RIkrKvF1qC67F"
+     this.weavy.tokenFactory = async (refresh: Boolean) => "https://simulat-e-learning-backend.onrender.com/token"
   }
+
 }
