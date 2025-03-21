833c59ca4dcc711a7659f0d7076b3b9d25baff3d
k-nitta-author <k.nitta.author@gmail.com>
2025-03-21 17:44:12 +0800

    removed reference to badge view component

--- SIMULAT-FRONTEND/src/app/general/account-page/account-page.component.ts
+++ SIMULAT-FRONTEND/src/app/general/account-page/account-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 
 import { AdminInfoViewComponent } from './admin-info-view/admin-info-view.component';
-import { BadgeViewComponent } from './badge-view/badge-view.component';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
 
 
 @Component({
   selector: 'app-account-page',
   standalone: true,
-  imports: [AdminInfoViewComponent, BadgeViewComponent, ProfileViewComponent],
+  imports: [AdminInfoViewComponent, ProfileViewComponent],
   templateUrl: './account-page.component.html',
   styleUrl: './account-page.component.css'
 })
